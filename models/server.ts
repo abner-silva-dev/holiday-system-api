24da5e5793e4599358639150446f1f2906ee1edc
abner-silva-dev <dylanabner14@gmail.com>
2024-10-29 13:41:08 -0600

    Dai system v.1.0.0

--- models/server.ts
+++ models/server.ts
@@ -27,9 +27,9 @@ class Server {
 
   middlewares() {
     // ABLE CORS
-    this.app.use(
+    const ip = this.app.use(
       cors({
-        origin: ["http://192.168.68.126:5173", "http://192.168.68.126:5173"],
+        origin: ["http://192.168.68.76:5173", "http://localhost:5173/"],
         methods: ["GET", "POST", "PATCH", "DELETE"], // Métodos permitidos
         credentials: true, // Si necesitas enviar cookies o autenticación
       })
